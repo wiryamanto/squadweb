332d07847ffa119a37f682023e6d47347dca6995
wiryamanto <wiryamantoragil@gmail.com>
2021-01-27 03:59:20 +0700

    update pertemuan 7

---
+++ pertemuan7/pengkondisian.js
@@ -0,0 +1,48 @@
+/**fungsi pengkondisian if else */ 
+//let plugin = 10;
+// if(plugin % 2 === 0){
+//     console.log(plugin +" adalah bilangan genap");
+// }else{
+//     console.log(plugin+"adalah bilangan ganjil");
+// }
+
+/**perulangan for */
+//for(i=1; i <= 12; i++){
+//    console.log("hello world!" +i);
+//}
+
+// let plugin=prompt("masukan angka : ")
+// if(plugin % 2 === 0){
+//          alert(plugin +" adalah bilangan genap");
+//      }else{
+//          alert(plugin+"adalah bilangan ganjil");
+//      }
+
+//while loop
+
+//  let coba = 1
+//  while(coba<=10){
+//      console.log("while loops ke -"+ coba);
+//      coba++
+//  }
+// let angka=false;
+// do{
+//     console.log("hello world!");
+
+// }
+// while(angka);
+
+function coba(){
+    console.log("fuction biasa");
+}
+
+coba();
+// fuction ekspresi
+//  let plugin =fuction(){
+//     .console.log("function ekspresi");
+// }
+
+let coba2 = ()=>{
+    console.log("Arrow function")
+}
+coba2();
